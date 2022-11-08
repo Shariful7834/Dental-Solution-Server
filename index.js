91d82c0860d3aa936b331410985705f1fd052750
Shariful Islam <engr.shariful.cse@gmail.com>
2022-11-09 00:50:54 +0100

    review data has posted

--- index.js
+++ index.js
@@ -23,12 +23,59 @@ async function run() {
       .db("DentalSolutionDB")
       .collection("services");
 
+    const reviewCollectioin = client
+      .db("DentalSolutionDB")
+      .collection("reviews");
+
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.limit(3).toArray();
       console.log(services);
       res.send(services);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    // Review api
+
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollectioin.insertOne(review);
+      console.log(result);
+      res.send(result);
+    });
+
+    app.get("/reviews", async (req, res) => {
+      console.log(req.query._id);
+      let query = {};
+      if (req.query.service) {
+        query = {
+          service: req.query.service,
+        };
+      }
+      const cursor = reviewCollectioin.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+
+    // get data by email query
+    app.get("/reviews", async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = reviewCollectioin.find(query);
+      const byemail = await cursor.toArray();
+      res.send(byemail);
     });
   } finally {
   }
